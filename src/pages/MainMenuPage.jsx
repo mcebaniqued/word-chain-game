afb4cebe61936386a9693c55645bf7832690f651
Miguel Baniqued <bmiguelcedric@gmail.com>
2025-04-21 23:34:29 -0700

    updated button css

--- src/pages/MainMenuPage.jsx
+++ src/pages/MainMenuPage.jsx
@@ -2,13 +2,15 @@ import { usePageContext } from "../context/PageContext";
 
 export const MainMenuPage = () => {
   const { changePage } = usePageContext();
+  const buttonStyle = 'text-xl px-4 py-2 rounded-lg border border-gray-300 hover:bg-neutral-800 transition-colors duration-300 cursor-pointer disabled:hover:bg-transparent disabled:opacity-50 disabled:cursor-not-allowed';
+
   return (
     <div className='flex flex-col w-full h-full justify-center items-center gap-8'>
       <h1 className='text-4xl'>Word Chain</h1>
 
       <button
         onClick={() => changePage('SelectDifficultyPage')}
-        className='text-2xl rounded-lg border border-gray-50'
+        className={buttonStyle}
       >
         Start
       </button>
--- src/pages/SelectDifficultyPage.jsx
+++ src/pages/SelectDifficultyPage.jsx
@@ -3,6 +3,7 @@ import { usePageContext } from "../context/PageContext";
 export const SelectDifficultyPage = () => {
   const { changePage, changeDifficulty } = usePageContext();
   const difficultyLevels = ['Easy', 'Medium', 'Hard']
+  const buttonStyle = 'text-xl px-4 py-2 rounded-lg border border-gray-300 hover:bg-neutral-800 transition-colors duration-300 cursor-pointer disabled:hover:bg-transparent disabled:opacity-50 disabled:cursor-not-allowed';
 
   return (
     <div className='flex flex-col w-full h-full justify-center items-center gap-8'>
@@ -16,7 +17,7 @@ export const SelectDifficultyPage = () => {
               changeDifficulty(levels);
               changePage('GamePage');
             }}
-          className='text-2xl px-4 py-2 rounded-lg border border-gray-300'
+          className={buttonStyle}
         >
           {levels}
         </button>
@@ -25,7 +26,7 @@ export const SelectDifficultyPage = () => {
 
       <button
         onClick={() => changePage('MainMenuPage')}
-        className='text-xl px-4 py-2'
+        className='cursor-pointer'
       >
         Return
       </button>
